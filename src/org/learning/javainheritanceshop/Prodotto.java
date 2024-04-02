b6934ac53978020e854d278bb124de23f59f2581
DavideGila <davide.gila@gmail.com>
2024-04-02 14:38:28 +0200

    class Product completed

--- src/org/learning/javainheritanceshop/Prodotto.java
+++ src/org/learning/javainheritanceshop/Prodotto.java
@@ -1,2 +1,50 @@
-package org.learning.javainheritanceshop;public class Prodotto {
+package org.learning.javainheritanceshop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Prodotto {
+    // ATTRIBUTI
+    private int code;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private BigDecimal vat;
+
+    // COSTRUTTORI
+
+    public Prodotto(String name, String brand, BigDecimal price, BigDecimal vat) {
+        this.code = generateCode();
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.vat = vat;
+    }
+
+
+    // GETTER E SETTER
+    public BigDecimal getPrice() {
+        return price.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public BigDecimal getFullPrice() {
+        // price + price * vat
+        return price.add(price.multiply(vat)).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public String getFullName() {
+        return String.format("%08d - %s", code, name);
+    }
+
+    public String getAllProductInformation(){
+        return String.format("Code: %08d\nName: %s\nBrand: %s\nPrice: %s\nVAT: %s\nFull Price: %s\nFull Name: %s",
+                code, name, brand, getPrice(), vat, getFullPrice(), getFullName());
+    }
+
+    // METODI
+    private int generateCode(){
+        Random random = new Random();
+        return random.nextInt(100000000);
+    }
 }
